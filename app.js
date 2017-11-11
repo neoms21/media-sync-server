6cff379f8b50ace9967eca51f2ea08850d242c11
Manoj Sethi <msethi3@sapient.com>
2017-11-11 21:01:18 +0000

    os determination

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var pictures = require('./routes/pictures');
 var files = require('./routes/files');
 
 var app = express();
-
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS, X-Requested-With');
--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
-var uploadDir = 'Y:';
-
-
+var os = require('os');
+var uploadDir = os.platform() !== 'darwin' ? 'Z:' : '/Users/mseth9/Movies';
 module.exports = {
     uploadDir: uploadDir
 };
